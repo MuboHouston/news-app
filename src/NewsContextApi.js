29da13aeca7c2176b0dc16e433dd044882ff4392
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-07-23 21:07:54 -0500

    add search function

--- src/NewsContextApi.js
+++ src/NewsContextApi.js
@@ -5,18 +5,42 @@ export const NewsContext = createContext()
 
 export const NewsContextProvider = (props) => {
     const [data, setData] = useState()
-    const apiKey = "45988b1a90db4c919e91bf44cabba346"
-    const query = "bitcoin"
+    const [isLoading, setIsLoading] = useState(true)
+    const [query, setQuery] = useState("");
+    const [searchInput, setSearchInput] = useState("")
+    const apiKey = "a8a9ac6e602045178b0f07b624b3256a"
+
+    const handleSubmit = e => {
+        //prevents the page from reloading
+        e.preventDefault();
+        setQuery(searchInput)
+    }
 
     useEffect(() => {
+        setIsLoading(false)
         axios.get(`https://newsapi.org/v2/everything?q=${query}&apiKey=${apiKey}`)
         .then(res => setData(res.data))
         .catch(err => console.log(err))
-    }, [data])
+    }, [data, query])
 
     return(
-        <NewsContext.Provider value={{ data }}>
-            {props.children}
-        </NewsContext.Provider>
+        <div> 
+            <form className="search-form" onSubmit={handleSubmit}>
+                <input 
+                    placeholder='Search for the news'
+                    value={searchInput}
+                    onChange={event => setSearchInput(event.target.value)}
+                />
+                <button type='submit'>Search</button>
+            </form>
+
+            {isLoading ? (
+                <p>Loading...</p> 
+            ) : (
+                <NewsContext.Provider value={{ data }}>
+                    {props.children}
+                </NewsContext.Provider>
+            )}
+        </div>
     )
 }
